1b9e200f69295e9ff24de61fff6e0be1ab2e1f9c
Plessy <ajonnel@gmail.com>
2020-02-17 12:10:22 -0800

    pdfreader: sample code

--- miner.py
+++ miner.py
@@ -1,7 +1,29 @@
 import pdfminer.high_level as pdf
+import PyPDF2
+from pdfreader import SimplePDFViewer
 
+# PDF Miner
 def test(str):
     text = pdf.extract_text(str)
-    return text
+    list = text.split()
+    return list
 
-report = test('file.pdf')
+# report = test('test1.pdf')
+
+# PyPDF2
+def test2(f):
+    file = PyPDF2.PdfFileReader(f)
+    text = file.getPage(0).extractText()
+    list = text.split()
+    return list
+
+# report1 = test2('test1.pdf')
+
+def reader(str):
+    file = open(str, 'rb')
+    view = SimplePDFViewer(file)
+    view.render()
+    pdf = view.canvas.strings
+    return pdf
+
+reader('test1.pdf')
