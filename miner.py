abc3714e3089942176815b1f075e0970766730a2
jonnel21 <ajonnel@gmail.com>
2020-02-19 00:06:02 -0800

    clean data

--- miner.py
+++ miner.py
@@ -1,6 +1,18 @@
 import pdfminer.high_level as pdf
 import PyPDF2
 from pdfreader import SimplePDFViewer
+from enum import Enum
+
+class Peak(Enum):
+    UNKNOWN = "Unknown"
+    VARIANT_WINDOW = "Variant-Window"
+    A1A = "A1a"
+    A1B = "A1b"
+    F = "F"
+    LA1C = "LA1c/CHb-1"
+    A1C = "A1c"
+    P3 = "P3"
+    A0 = "A0"
 
 # PDF Miner
 def test(str):
@@ -19,11 +31,19 @@ def test2(f):
 
 # report1 = test2('test1.pdf')
 
+#pdfreader
 def reader(str):
     file = open(str, 'rb')
     view = SimplePDFViewer(file)
     view.render()
     pdf = view.canvas.strings
-    return pdf
+    start = pdf.index('Area % ') # inclusive
+    end = pdf.index('Total Area: ') # exclusive
+    peak_table = pdf[start + 1 : end]
+    
+    return peak_table
 
-reader('test1.pdf')
+def parse(table):
+    
+reader('test.pdf')
+
