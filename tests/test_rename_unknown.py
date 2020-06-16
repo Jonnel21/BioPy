9869928e55de25e9aa85651a72fc8b42a7804245
Plessy <ajonnel@gmail.com>
2020-06-16 11:51:09 -0700

    Add unit test for rename_unknown function

---
+++ tests/test_rename_unknown.py
@@ -0,0 +1,30 @@
+import unittest
+from device.instrument import InstrumentStrategy as inst
+
+class RenameUnknownTestCase(unittest.TestCase):
+    ''' tests the rename_unknown function'''
+
+    def test_rename_unknown_happy_path(self):
+        test_list = ['Jonnel', 'Unknown', 'one']
+        expected_result = ['Jonnel', 'Unknown1', 'one']
+        inst.rename_unknown(self, test_list)
+        self.assertEqual(test_list, expected_result)
+
+    def test_rename_unknown_multiple_unknowns(self):
+        test_list = ['Unknown', 'F', 'Unknown', 'A1c', 'Unknown']
+        expected_result = ['Unknown1', 'F', 'Unknown2', 'A1c', 'Unknown3']
+        inst.rename_unknown(self, test_list)
+        self.assertEqual(test_list, expected_result)
+
+    def test_rename_unknown_empty_list(self):
+        test_list = []
+        expected_result = 'List is empty.'
+        self.assertEqual(inst.rename_unknown(self, test_list), expected_result)
+
+    def test_rename_unknown_no_unknowns(self):
+        test_list = ['F', 'A1c', 'S', 'E', 'LA1c']
+        expected_result = 'There are no Unknowns in the list.'
+        self.assertEqual(inst.rename_unknown(self, test_list), expected_result)
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_wrapper_decode.py
@@ -0,0 +1,22 @@
+import unittest
+from device.instrument import InstrumentStrategy as inst
+
+class WrapperDecodeTestCase(unittest.TestCase):
+    ''' tests for the wrapper_decode function'''
+
+    def test_wrapper_decode_empty_array(self):
+        arr_to_test = []
+        self.assertFalse(inst.wrapper_decode(self, arr_to_test))
+
+    def test_wrapper_decode_happy_path(self):
+        arr_to_test = [b'one', b'two', b'three']
+        expected_result = ['one', 'two', 'three']
+        self.assertEqual(expected_result, inst.wrapper_decode(self, arr_to_test))
+
+    def test_wrapper_decode_sad_path(self):
+        arr_to_test = [b'one', b'two', b'three']
+        expected_result = ['one1', 'two2', 'three3']
+        self.assertNotEqual(expected_result, inst.wrapper_decode(self, arr_to_test))
+
+if __name__ == '__main__':
+    unittest.main()
